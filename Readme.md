a0187af7b3a197e888fa9ffa8875532292bce38a
jitunmohajan <jitunmohajan@gmail.com>
2023-08-13 12:15:38 +0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+npx ts-node index.ts
+
+npx prisma migrate dev
+
+
---
+++ index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(){
+    const getAllUsers = await prisma.user.findMany();
+    console.log(getAllUsers);
+
+    // const postUsers = await prisma.user.create({
+    //     data:{
+    //         email: "asd1@gmail.com",
+    //         name: "asd1"
+    //     }
+    // })
+}
+
+main();
