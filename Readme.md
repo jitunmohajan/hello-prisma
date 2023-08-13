4a4d0ce8be09e8c9c709c870f1c83fcc61eff59f
jitunmohajan <jitunmohajan@gmail.com>
2023-08-13 13:11:04 +0600

    adding modules

--- Readme.md
+++ Readme.md
@@ -3,3 +3,14 @@ npx ts-node index.ts
 npx prisma migrate dev
 
 npx prisma generate
+
+
+yarn add express cors
+
+
+```
+"scripts": {
+    "start": "nodemon ./src/index.ts"
+},
+
+```
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+import app from './app'
+
+const prisma = new PrismaClient();
+const port = process.env.PORT || 3003;
+
+async function main(){
+   app.listen(port, ()=>{
+        console.log(`Server listening on port ${port}`);
+   }) 
+}
+
+main();
