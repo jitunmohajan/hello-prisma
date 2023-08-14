0c74fb25c22942a7833434013057f149d1f8782c
jitunmohajan <jitunmohajan@gmail.com>
2023-08-14 19:52:41 +0600

    create-category

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express,{Application} from "express";
 import cors from "cors";
 import { UserRoutes } from "./modules/user/user.route";
+import { CategoryRoutes } from "./modules/category/category.route";
 
 
 const app: Application = express();
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.urlencoded({extended: true}))
 
 app.use("/api/v1/user",UserRoutes)
+app.use("/api/v1/category",CategoryRoutes)
 
 export default app;
