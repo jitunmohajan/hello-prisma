3b4f7c28ae90ef8e8f70f3cac298195bf630a80d
jitunmohajan <jitunmohajan@gmail.com>
2023-08-15 11:34:30 +0600

    adding pagination

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -14,14 +14,36 @@ const createPost = async(data:Post):Promise<Post>=>{
 }
 
 const getAllPost =async(options:any)=>{
-    const { sortBy, sortOrder } = options;
+    const { sortBy, sortOrder, searchTerm, page, limit } = options;
+    const skip = parseInt(limit)* parseInt(page)- parseInt(limit);
+    const take = parseInt(limit)
     const result  = await prisma.post.findMany({
+        skip,
+        take,
         include:{
             author:true,
             category:true
         },
-        orderBy:{
+        orderBy: sortBy && sortOrder? {
             [sortBy]: sortOrder
+        } : { createdAt: 'desc'},
+        where:{
+            OR:[
+                {
+                    title:{
+                        contains: searchTerm,
+                        mode:'insensitive'
+                    }
+                },
+                {
+                    author:{
+                        name:{
+                            contains: searchTerm,
+                            mode:'insensitive'
+                        }
+                    }
+                }
+            ]
         }
     });
     return result;
